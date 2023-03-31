9fc4d22ba1d9a786f5023241ce58880d1f14f00e
nftchance <chance@utc24.io>
2023-03-31 15:49:54 -0500

    fix: button list

--- frontend/src/pages/Buttons.tsx
+++ frontend/src/pages/Buttons.tsx
@@ -14,9 +14,20 @@ const Buttons = () => {
         error: any;
         data: any;
     } = useQuery({
-        queryKey: ["button"],
+        queryKey: ["buttons"],
         queryFn: () => fetch(`http://localhost:8000/buttons/`).then((res) => res.json())
     });
+
+    const handleDelete = (buttonId: number) => {
+        fetch(`http://localhost:8000/buttons/${buttonId}/`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(() => {
+            console.log(`Deleted button ${buttonId}`)
+        })
+    }
 
     if (isLoading) return <p>Loading...</p>;
 
@@ -33,9 +44,9 @@ const Buttons = () => {
             <ul>
                 {data?.length > 0 ? data.map((button: any) => (
                     <li key={button.id}>
-                        <Link to={`/button/${button.id}/`}>{button.text}</Link>
-                        <span> Edit </span>
-                        <span> Delete </span>
+                        <Link to={`/button/${button.id}/edit/`}>{button.text}</Link>
+                        <Link to={`/button/${button.id}/`}>View</Link>
+                        <button onClick={() => handleDelete(button.id)}>Delete</button>
                     </li>
                 )) : <p>No buttons yet!</p>}
             </ul>
