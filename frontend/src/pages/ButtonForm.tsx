8bcc9739d0d50f701c035b3ad53343f8e13e1fe1
nftchance <chance@utc24.io>
2023-03-31 16:09:51 -0500

    feat: connected form

--- frontend/src/pages/ButtonForm.tsx
+++ frontend/src/pages/ButtonForm.tsx
@@ -26,11 +26,17 @@ const ButtonForm = ({ isEdit }: { isEdit?: boolean }) => {
 
     const [object, setObject] = useState<any>(data);
 
+    const handleDelete = () => {
+        fetch(`http://localhost:8000/buttons/${buttonId}/`, {
+            method: "DELETE",
+        }).then(() => {
+            console.log("Deleted!")
+        })
+    }
+
     if (isEdit && isLoading) return <p>Loading...</p>;
 
     if (isEdit && error) return <p>Error: {error.message}</p>;
-
-    console.log(object);
 
     return (
         <>
@@ -75,12 +81,22 @@ const ButtonForm = ({ isEdit }: { isEdit?: boolean }) => {
                             name="primaryColor"
                             id="primaryColor"
                             placeholder="Primary color..."
-                            defaultValue={object?.primaryColor}
+                            defaultValue={data?.primary_color}
+                            value={object?.primary_color}
+                            onChange={(e) => setObject({ ...object, primary_color: e.target.value })}
                         />
                     </div>
 
                     <div>
-                        <input type="text" name="secondaryColor" id="secondaryColor" placeholder="Secondary color..." />
+                        <input
+                            type="text"
+                            name="secondaryColor"
+                            id="secondaryColor"
+                            placeholder="Secondary color..."
+                            defaultValue={data?.secondary_color}
+                            value={object?.secondary_color}
+                            onChange={(e) => setObject({ ...object, secondary_color: e.target.value })}
+                        />
                     </div>
                 </div>
 
@@ -88,7 +104,7 @@ const ButtonForm = ({ isEdit }: { isEdit?: boolean }) => {
 
                 <hr />
 
-                <div>
+                <div className="input-append">
                     <input type="text" name="search" id="search" placeholder="Search..." />
                     <button type="button">Search</button>
                 </div>
@@ -98,7 +114,14 @@ const ButtonForm = ({ isEdit }: { isEdit?: boolean }) => {
                         <input type="checkbox" name="token" id={`token-${token.id}`} />
                         <label htmlFor={`token-${token.id}`}>{token.name}</label>
                     </div>
-                )) : <p>No tokens found...</p>}
+                )) : <p>No tokens targeted...</p>}
+
+                {isEdit && <button
+                    type="button"
+                    onClick={handleDelete}
+                >Delete</button>}
+
+                <button type="submit">Save</button>
             </form>
         </>
     )
