3b851c486bac5faaed754f78b01f7786eb6ec673
nftchance <chance@utc24.io>
2023-04-12 16:04:14 -0500

    feat: blockchain images

--- frontend/src/components/Button/ButtonCard.tsx
+++ frontend/src/components/Button/ButtonCard.tsx
@@ -1,9 +1,19 @@
 import { Link } from 'react-router-dom'
 
+import { BlockchainImages } from "../Images/BlockchainImages"
+
+import "./ButtonCard.css"
+
 const ButtonCard = ({ button }: any) => {
+    console.log(button)
+
+    const blockchains: unknown[] = [...new Set(button.tokens.map((token: any) => token.blockchain))]
+
     return <Link to={`/account/buttons/${button.id}/`}>
-        <div className="card">
-            <h4>{button.name}</h4>
+        <div className="button-card card">
+            <p>{button.name}</p>
+
+            <BlockchainImages blockchains={blockchains} />
         </div>
     </Link>
 }
--- frontend/src/pages/Buttons.tsx
+++ frontend/src/pages/Buttons.tsx
@@ -30,8 +30,6 @@ const Buttons = () => {
 
     return (
         <>
-            <ProfileCard address={address} />
-
             <Link
                 to="/account/buttons/new/"
                 children={<button className="primary secondary block">
