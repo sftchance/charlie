a8c45a58a516565a32ebe223c11c6468455c465f
nftchance <chance@utc24.io>
2023-04-20 20:37:09 -0500

    fix: empty row title

--- frontend/src/components/Button/ButtonRow.tsx
+++ frontend/src/components/Button/ButtonRow.tsx
@@ -5,7 +5,7 @@ const ButtonRow = ({ buttons }: any) => {
         {buttons?.length > 0 ? buttons.map((button: any) => (
             <ButtonCard key={button.id} button={button} />
         )) : <Card className="hero">
-            <h2>No buttons found.</h2>
+            <h2>No buttons created yet.</h2>
             <p>Click the <strong>Create button</strong> above to get started!</p>
         </Card>}
     </>
